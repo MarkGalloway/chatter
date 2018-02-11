05f47b4832934144ffe009a0409b91cfa9e9c89b
Mark Galloway <mgallowa@ualberta.ca>
2018-02-10 19:43:45 -0700

    Add Jest testing

---
+++ src/__tests__/schema.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+
+const App = require('../app');
+
+let server;
+
+beforeAll(() => {
+  const app = new App();
+  server = app.callback();
+});
+
+it('Should say hello', async () => {
+  const expectedValue = { data: { hello: 'Hello World.' } };
+
+  const query = `
+    query hello {
+      hello
+    }`;
+
+  const response = await request(server)
+    .post('/graphql')
+    .send({ query });
+
+  expect(response.body.errors).toBeUndefined();
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual(expectedValue);
+  expect.assertions(3);
+});
