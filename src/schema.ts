e733411eccd90a1d929b87da0b2c4ea693879661
Mark Galloway <mgallowa@ualberta.ca>
2018-03-26 21:50:24 -0600

    Date Scalar type

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,7 @@
-import { GraphQLCompositeType } from 'graphql';
+import { GraphQLScalarType } from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
+import { Kind } from 'graphql/language';
+import moment from 'moment';
 
 const typeDefs = [
   `
@@ -10,6 +12,24 @@ type Query {
 ];
 
 const resolvers = {
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date',
+    parseValue(value) {
+      return moment(value).toDate();
+    },
+    serialize(value) {
+      return moment(value).format();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.STRING) {
+        const date = moment(ast.value);
+        if (date.isValid()) {
+          return date.toDate();
+        }
+      }
+    },
+  }),
   Query: {
     hello: (root: any, args: object, context: object) => 'Hello World.',
   },
