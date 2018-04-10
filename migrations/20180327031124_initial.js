981774249ad77b366701ba650f62e7968423658e
Mark Galloway <mgallowa@ualberta.ca>
2018-04-09 21:07:13 -0600

    Add initial replies schema

--- migrations/20180327031124_initial.js
+++ migrations/20180327031124_initial.js
@@ -1,31 +1,70 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('topics', table => {
-    table.increments('id').primary();
+  return Promise.all([
+    knex.schema.createTable('topics', table => {
+      table.increments('id').primary();
 
-    // TODO: FK
-    table.integer('author').notNullable();
+      // TODO: FK
+      table
+        .integer('author')
+        .unsigned()
+        .notNullable();
 
-    table.text('body').notNullable();
+      table.text('body').notNullable();
 
-    table
-      .enu('status', ['visible', 'archived'])
-      .notNullable()
-      .defaultTo('visible');
+      table
+        .enu('status', ['visible', 'archived'])
+        .notNullable()
+        .defaultTo('visible');
 
-    table
-      .timestamp('createdDate')
-      .notNullable()
-      .defaultTo(knex.fn.now());
+      table
+        .timestamp('createdDate')
+        .notNullable()
+        .defaultTo(knex.fn.now());
 
-    table
-      .timestamp('updatedDate')
-      .notNullable()
-      .defaultTo(knex.fn.now());
+      table
+        .timestamp('updatedDate')
+        .notNullable()
+        .defaultTo(knex.fn.now());
 
-    table.index(['status', 'author']);
-  });
+      table.index(['status', 'author']);
+    }),
+    knex.schema.createTable('replies', table => {
+      table.increments('id').primary();
+
+      // TODO: FK
+      table
+        .integer('author')
+        .unsigned()
+        .notNullable();
+
+      table
+        .integer('topic_id')
+        .references('topics.id')
+        .unsigned()
+        .notNullable();
+
+      table.text('body').notNullable();
+
+      table
+        .enu('status', ['visible', 'archived'])
+        .notNullable()
+        .defaultTo('visible');
+
+      table
+        .timestamp('createdDate')
+        .notNullable()
+        .defaultTo(knex.fn.now());
+
+      table
+        .timestamp('updatedDate')
+        .notNullable()
+        .defaultTo(knex.fn.now());
+
+      table.index(['status', 'author', 'topic_id']);
+    }),
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('topics');
+  return knex.schema.dropTableIfExists('topics').dropTableIfExists('replies');
 };
